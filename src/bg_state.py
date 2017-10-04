76b6dee8e7a3ddcecc6f4b526aabf4a4e25af4e7
brainshunt <brainshunt@circushomonovus.org>
2017-10-04 13:58:36 +0200

    make move validity checker more readable
     * own points class
     * fix bug in .gitignore

--- src/bg_state.py
+++ src/bg_state.py
@@ -1,3 +1,9 @@
+class points:
+    def __init__(self):
+        self.color = None
+        self.count = 0
+
+
 class move:
     def __init__(self, source, target):
         """ Represents a move: Consists of source and target """
@@ -33,12 +39,16 @@ class state:
     def __init__(self, state_array, current_player):
         """ This class represents the state of a backgammon game """
         if state_array is None:
-            self.tokens = [[0 for y in range(2)] for x in range(25)]
-            self.tokens[1][0] = 'black'
+            self.tokens = [points() for x in range(25)]
+            # [[0 for y in range(2)] for x in range(25)]
+            self.tokens[1].color = 'black'  # only for testing
         else:
             self.tokens = state_array
         self.dice_roll = [0] * 2
         self.current_player = current_player
+
+    def get_state(self):
+        return self.tokens
 
     def change_state(self):
         """ Executes a move """
@@ -62,17 +72,25 @@ class state:
         >>> mv = move(1, 5)
         >>> st1.proposed_move_valid(mv)
         True
+        >>> mv = move(2, 5)
+        >>> st1. proposed_move_valid(mv)
+        Invalid move: Player tries to move opponents or no token.
+        False
 
         """
-        if self.current_player != self.tokens[move.source][0]:
+        if self.current_player != self.tokens[move.source].color:
+            print("Invalid move: Player tries to move opponents or no token.")
             return False
-        if self.tokens[move.source][0] != self.tokens[move.target][0] \
-           and self.tokens[move.target][1] > 1:
+        if self.tokens[move.source].color != self.tokens[move.target].color \
+           and self.tokens[move.target].count > 1:
+            print("Invalid move: Player tries kick multiple opponents tokens.")
             return False
-        if self.tokens[move.source][0] == self.tokens[move.target][0] \
-           and self.tokens[move.target][1] > 4:
+        if self.tokens[move.source].color == self.tokens[move.target].color \
+           and self.tokens[move.target].count > 4:
+            print("Invalid move: Player tries to move to a full house.")
             return False
         if move.target > 24:
+            print("Invalid move: Player tries to move beyond the board.")
             return False
         # otherwise, move is valid
         return True
